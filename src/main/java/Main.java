836c6f6fbcc4b41bf66f286b052b15bd6d7dc2c9
agnes <agnes.hinterplattner@gmx.at>
2021-01-21 17:57:25 +0100

    CeasarChallenge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+
 public class Main {
 
+
     public static void main(String[] args){
-        System.out.println("Hi!");
+
+        String code = "QbanHqnzCsFpuvssnuEGFtrFryyFpunsGFxnCvGnrA";
+
+        System.out.println("Alle möglichen Verschiebungen von " + code + ":\n");
+        entschluesseln(code);
+
     }
+
+
+    static String range = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+
+    public static String entschluesseln(String text)
+    {
+       String loesungswort = text +  "      WHAAAAAT? bitte übersetzen!!";
+       System.out.println(loesungswort);
+       return loesungswort;
+    }
+
+
 }
+
