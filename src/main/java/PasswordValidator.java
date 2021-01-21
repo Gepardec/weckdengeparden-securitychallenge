cc4d6391d5299279d3e75115c730ec514e640d34
PhilippJKU <p.engelbrechtsmueller@gmx.at>
2021-01-21 15:22:37 +0100

    #105 Klasse PasswordValidator erstellt

---
+++ src/main/java/PasswordValidator.java
@@ -0,0 +1,17 @@
+
+
+public class PasswordValidator {
+
+   /*
+   * Wie du gesehen hast sind die Passwörter in unserer Datenbank sehr einfach.
+   * Kannst du uns helfen einen Validator zu schreiben der Passwörter auf folgende Eigenschaften prüft?
+   *
+   * Minimale Länge: 5
+   * Maximale Länge: 10
+   * Enthält zumindest EINE Zahl
+   * Enthält zumindest EIN Sonderzeichen (&, +, @, $, #, %, etc.)
+   * Enthält KEINE Leerzeichen
+   *
+   * */
+
+}
