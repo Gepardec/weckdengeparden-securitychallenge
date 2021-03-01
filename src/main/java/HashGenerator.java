fc0f4db6c7ee45b6bbd43c3e027ebf34a413cdb2
cbacon93 <subcortexx@gmail.com>
2021-03-01 09:11:54 +0100

    Add hash function

--- src/main/java/HashGenerator.java
+++ src/main/java/HashGenerator.java
@@ -30,19 +30,14 @@ public class HashGenerator {
           bytes.add((byte)(0xff & a));
         }
 
-        int h0 = 0x67452301;
-        int h1 = 0xEFCDAB89;
-        int h2 = 0x98BADCFE;
-        int h3 = 0x10325476;
-        int h4 = 0xC3D2E1F0;
         long ml = toHash.length()*8;
 
-        //prepare
+        //make msg multiple of 64 bytes (-8 bytes)
         bytes.add((byte)0x80);
         while (bytes.size() % 64 != 56) {
           bytes.add((byte)0x00);
         }
-        //add 64 bit msg length
+        //add 8 byte msg length
         bytes.add((byte)((ml >> 56) & 0xFF));
         bytes.add((byte)((ml >> 48) & 0xFF));
         bytes.add((byte)((ml >> 40) & 0xFF));
@@ -52,15 +47,97 @@ public class HashGenerator {
         bytes.add((byte)((ml >> 8) & 0xFF));
         bytes.add((byte)((ml >> 0) & 0xFF));
 
+        int h0 = 0x67452301;
+        int h1 = 0xEFCDAB89;
+        int h2 = 0x98BADCFE;
+        int h3 = 0x10325476;
+        int h4 = 0xC3D2E1F0;
 
-        //debug output
-        String o = "";
-        for (byte b : bytes) {
-          o += String.format("%02X ", b);
+        //for each 64 byte fragment
+        for(int p=0; p < bytes.size(); p+= 64)
+        {
+          //generate 80 x 32 bit words by xoring
+          ArrayList<Integer> intlist = fragmentToInteger(bytes, p);
+          for (int i=16; i < 80; i++) {
+            int w = leftRotate(intlist.get(i-3) ^ intlist.get(i-8) ^ intlist.get(i-14) ^ intlist.get(i-16), 1);
+            intlist.add(w);
+          }
+
+          //init fragment values
+          int a = h0;
+          int b = h1;
+          int c = h2;
+          int d = h3;
+          int e = h4;
+          int f = 0;
+          int k = 0;
+
+          //do the hashing work
+          for (int i = 0; i < 80; i++) {
+            if (i < 20) {
+              f = (b & c) | ((~b) & d);
+              k = 0x5A827999;
+            } else if (i < 40) {
+              f = b ^ c ^ d;
+              k = 0x6ED9EBA1;
+            } else if (i < 60) {
+              f = (b & c) | (b & d) | (c & d);
+              k = 0x8F1BBCDC;
+            } else {
+              f = b ^ c ^ d;
+              k = 0xCA62C1D6;
+            }
+
+            int temp = leftRotate(a, 5) + f + e + k + intlist.get(i);
+            e = d;
+            d = c;
+            c = leftRotate(b, 30);
+            b = a;
+            a = temp;
+          }
+
+          // add up fragment values
+          h0 += a;
+          h1 += b;
+          h2 += c;
+          h3 += d;
+          h4 += e;
         }
-        System.out.println(o);
+
+        //produce final hash
+        String hash = "";
+        hash += String.format("%08X", h0);
+        hash += String.format("%08X", h1);
+        hash += String.format("%08X", h2);
+        hash += String.format("%08X", h3);
+        hash += String.format("%08X", h4);
+        return hash;
+    }
+
+    /**
+     * Breaks up 64 x 8 bit fragment into 16 x 32 bit word list
+     * @param  bytes byte array
+     * @param  p     the fragment pointer
+     * @return       the 16 x 32 bit word list
+     */
+    static ArrayList<Integer> fragmentToInteger(ArrayList<Byte> bytes, int p)
+    {
+      ArrayList<Integer> intlist = new ArrayList<Integer>();
+      for (int i=p; i < p+64; i+=4) {
+        int insert = 0x00000000;
+        insert |= (bytes.get(i) & 0xff) << 24;
+        insert |= (bytes.get(i+1) & 0xff) << 16;
+        insert |= (bytes.get(i+2) & 0xff) << 8;
+        insert |= (bytes.get(i+3) & 0xff);
+        intlist.add(insert);
+      }
+
+      return intlist;
+    }
 
 
-        return String.valueOf(bytes.size());
+    static int leftRotate(int input, int shift)
+    {
+      return (input << shift) | (input >>> (32 - shift));
     }
 }
